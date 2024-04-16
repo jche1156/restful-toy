c3b27065a9982bea035259017a432d4289080b95
John Chen <john.hk.chen@gmail.com>
2024-04-16 13:41:58 -0700

    Add HTMX reactivity

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ import { createClient } from '@supabase/supabase-js'
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = 7890;
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(express.static("public")); 
 
-const supabaseUrl = 'https://reamqvucjdwylegwdnai.supabase.co'; //supabase details
+const supabaseUrl = process.env.SUPABASE_URL; //supabase details
 const supabaseKey = process.env.SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
@@ -19,19 +19,40 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 let items = [];
 
 app.get("/", async (req, res) => {
-
     try {
         const { data, error } = await supabase
             .from('run')
             .select('*');
-        items = data;
-
         res.render("index.ejs", {
             projectTitle: "Run Log",
-            runList: items,
+            runList: data,
         });
     } catch(err) {
         console.error(err);
+    }
+});
+
+app.post("/rest/hi", async (req, res) => {
+    try {
+        const { data, error } = await supabase.from('run').insert([
+            {
+                location: "CCSF Track",
+                distance: "4",
+                pace: "11:12",
+                comments: "whoooosh",
+            },
+        ]);
+
+        const newest_item = await supabase
+            .from('run')
+            .select("*") //columns to select from the database
+            .order('id', { ascending: false })
+            .limit(1);
+        res.render("parts/run.ejs", {
+            item: newest_item.data[0]
+        });    
+    } catch(err) {
+        console.log(err);
     }
 });
 
